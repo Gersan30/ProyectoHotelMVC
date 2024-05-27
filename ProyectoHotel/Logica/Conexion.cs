8b98884fd9103ddd34049306c78ee411a86a1996
GerGit <Gergit@proyGit.com>
2024-05-27 10:51:49 -0600

    Se agrega proyecto

---
+++ ProyectoHotel/Logica/Conexion.cs
@@ -0,0 +1,13 @@
+﻿//AQUI HACEMOS LA CONEXION A LA BASE DE DATOS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoHotel.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=.;Initial Catalog=DB_HOTEL;Integrated Security=True";
+    }
+}
